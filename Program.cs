6cb7e38dae74690c167af45e32c35c7993599240
matejmusap <matej.musap@gmail.com>
2020-07-27 01:23:39 +0200

    Implement Serilog

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using Serilog;
+
 
 namespace TaxiCustomerAlgorithm
 {
@@ -7,12 +8,22 @@ namespace TaxiCustomerAlgorithm
     {
         static void Main()
         {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs//taxicustomeralgorithm.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
             var random = new Random();
-            int LowestPrice = 0;
+            int LowestExpense = 0;
             string[] Customers = { "Customer1", "Customer2", "Customer3" };
             int[] CustomersExpenses1 = { random.Next(200), random.Next(200), random.Next(200) };
-            int[] CustomersExpenses2 = { random.Next(20), random.Next(20), random.Next(20) };
-            int[] CustomersExpenses3 = { random.Next(20), random.Next(20), random.Next(20) };
+            int[] CustomersExpenses2 = { random.Next(200), random.Next(200), random.Next(200) };
+            int[] CustomersExpenses3 = { random.Next(200), random.Next(200), random.Next(200) };
+            Log.Information("Customer1 expanses  for [Taxi 1, Taxi 2, Taxi 3]: {CustomersExpenses1}", CustomersExpenses1);
+            Log.Information("Customer2 expanses for [Taxi 1, Taxi 2, Taxi 3]: {CustomersExpenses2}", CustomersExpenses2);
+            Log.Information("Customer3 expanses for [Taxi 1, Taxi 2, Taxi 3]: {CustomersExpenses3}", CustomersExpenses3);
+
             string Result = $"Taxi 1 : {Customers[0]} for {CustomersExpenses1[0]} USD, Taxi 2 : {Customers[1]} for {CustomersExpenses1[1]} USD, Taxi 3 : {Customers[2]} for {CustomersExpenses1[2]} USD";
 
             for (int i1 = 0; i1 < CustomersExpenses1.Length; i1++)
@@ -32,27 +43,21 @@ namespace TaxiCustomerAlgorithm
                             continue;
                         }
                         int sumPrice3 = sumPrice2 + CustomersExpenses3[i3];
-                        if (LowestPrice == 0 || sumPrice3 < LowestPrice)
+                        Log.Information("If Current {LowestPrice} is bigger than {sumPrice3} or {LowestPrice} is equal to 0, LowestPrice value will be changed", LowestExpense, sumPrice3);
+                        if (LowestExpense == 0 || sumPrice3 < LowestExpense)
                         {
-                            LowestPrice = sumPrice3;
-                            Result = $"Taxi 1 : {Customers[i1]} for {CustomersExpenses1[i1]} USD, Taxi 2 : {Customers[i2]} for {CustomersExpenses2[i2]} USD, Taxi3 : {Customers[i3]} for {CustomersExpenses3[i3]} USD";
+                            LowestExpense = sumPrice3;
+                            Log.Information("New Lowest Expense for company is {LowestExpense}", LowestExpense);
+                            Log.Information("{i1}, {i2}, {i3} are indexex that defines current Lowest Expense: {LowestExpense}!", i1, i2, i3, LowestExpense);
+                            Result = $"Lowest expense for company is Taxi 1 : {Customers[i1]} for {CustomersExpenses1[i1]} USD, Taxi 2 : {Customers[i2]} for {CustomersExpenses2[i2]} USD, Taxi3 : {Customers[i3]} for {CustomersExpenses3[i3]} USD";
                         }
                     };
                 };
             };
+            Log.Information("{Result}", Result);
+            Log.Information("Lowest expense is {LowestExpense}", LowestExpense);
             Console.WriteLine($"{Result}");
-            Console.WriteLine($"Lowest combined price is {LowestPrice}");
+            Console.WriteLine($"Lowest expense is {LowestExpense}");
         }
     }
 }
-
-/*tri customera puta tri taksista je 9 troškova
-svaki takist izabare jednog customera
-1 takist prvi trošak => ista 3 troška => 1 takista 1 trošak => 9 KOMB
-1 takist drugi trošak => ista 3 troška* => 1 takista 2 trošak => 9 KOMB
-1 takist treći tošak => ista 3 troška* => 1 takista 3 trošak => 9 KOMB
-zbroj troškova
-dobiti prvi trošak
-ako je idući trošak veći odbaci, ako je manji postaje prvi
-izabrati najmanji trošak
-vratiti taksix customerx taksiy customery taksiz customerz*/
